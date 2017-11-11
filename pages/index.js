627669d4a8e2ad4073e07db8f23a0d38385fcbe4
Daniella Cocco <d@daniellacoc.co>
2017-11-11 11:32:08 +0100

    Favicons

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,15 @@ export default () =>
       <title>Remains of the Republic | SWBF2 gaming clan</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <link rel='stylesheet' type='text/css' href='/static/normalize.min.css' />
+      <meta name='msapplication-config' content='/static/browserconfig.xml' />
+      <meta name='theme-color' content='#ffffff' />
+      <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png?v=OmybA708yg' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png?v=OmybA708yg' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png?v=OmybA708yg' />
+      <link rel='manifest' href='/static/manifest.json?v=OmybA708yg' />
+      <link rel='mask-icon' href='/static/safari-pinned-tab.svg?v=OmybA708yg' color='#1454a1' />
+      <link rel='shortcut icon' href='/static/favicon.ico?v=OmybA708yg' />
+      <link rel='stylesheet' type='text/css' href='/static/normalize.min.css?v=OmybA708yg' />
     </Head>
     <Intro />
     <style jsx>{`
